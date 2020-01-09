e45886679cd177986654b21ee93971ecbffb59fe
AbhishekKr <abhikumar163@gmail.com>
2020-01-09 11:43:40 +0530

    update go.mod; fix behavioral test/example
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- _tests_behavioral_/pickler.go
+++ _tests_behavioral_/pickler.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 
 	"github.com/abhishekkr/ficklepickle"
+	"github.com/abhishekkr/ficklepickle/config"
+
+	golassert "github.com/abhishekkr/gol/golassert"
 )
+
+func init() {
+	config.PickleDir = "./_tests_behavioral_"
+}
 
 type Person struct {
 	Name    string `json:"fullname"`
 	Address string
-	phone   Phone
+	Xphone  Phone
 }
 
 type Phone struct {
@@ -17,15 +24,43 @@ type Phone struct {
 	Office string
 }
 
+func testPickleUnpickle(johnny Person) {
+	pickle, err := ficklepickle.Pickle(johnny)
+	golassert.AssertEqual(err, nil)
+
+	j := Person{}
+	err = ficklepickle.Unpickle(pickle, &j)
+	golassert.AssertEqual(err, nil)
+
+	golassert.AssertEqual(johnny.Name, j.Name)
+	golassert.AssertEqual(johnny.Address, j.Address)
+	golassert.AssertEqual(johnny.Xphone.Home, j.Xphone.Home)
+	golassert.AssertEqual(johnny.Xphone.Office, j.Xphone.Office)
+}
+
+func testReadWrite(johnny Person) {
+	johnny.Xphone = Phone{Home: "010101"}
+
+	err := ficklepickle.Write("vanilla-file", "johnny", johnny)
+	fmt.Println("~write:")
+	fmt.Println(johnny)
+	golassert.AssertEqual(err, nil)
+
+	j := Person{}
+	err = ficklepickle.Read("vanilla-file", "johnny", &j)
+	fmt.Println("~read:")
+	fmt.Println(j)
+	golassert.AssertEqual(err, nil)
+
+	golassert.AssertEqual(johnny.Name, j.Name)
+	golassert.AssertEqual(johnny.Address, j.Address)
+	golassert.AssertEqual(johnny.Xphone.Home, j.Xphone.Home)
+	golassert.AssertEqual(johnny.Xphone.Office, j.Xphone.Office)
+}
+
 func main() {
 	johnny := Person{Name: "Johnny", Address: "Wherever"}
-	fmt.Println(johnny)
-
-	pickle, err := ficklepickle.Pickle(johnny)
-	fmt.Println(pickle)
-	fmt.Println(err)
-
-	j, err := ficklepickle.Unpickle(pickle, Person{})
-	fmt.Println(j)
-	fmt.Println(err)
+	testPickleUnpickle(johnny)
+	testReadWrite(johnny)
+	fmt.Println("done.")
 }
